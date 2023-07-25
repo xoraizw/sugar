be3237c71695f8215b256bffbc5db3d51dd188f7
xoraizw <xoraizw@gmail.com>
2023-07-25 13:13:45 +0500

    links update

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,14 +13,14 @@ export default function Header(props) {
       <Navbar expand="sm" style={{ backgroundColor: "#f27d64", fontFamily: 'Roboto, sans-serif', color: "#f4eb7b", borderBottom: "2px solid #ec8013", boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)" }}>
         <Container className="d-flex justify-content-between align-items-center">
           <div>
-            <Navbar.Brand href="#home">
+            <Navbar.Brand href="/sugar">
               <img src={SugarRushIcon} height="80px" className="m-1 p-1" alt="MDB Logo" loading="lazy" />
               <span style={{ color: "#f4eb7b" }}>SugarRush</span>
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link href="/" style={{color: "#f4eb7b"}}>Home</Nav.Link>
+                <Nav.Link href="/sugar" style={{color: "#f4eb7b"}}>Home</Nav.Link>
                 <Nav.Link as={Link} smooth to="menu-section" duration={50} style={{ cursor: 'pointer', color: "#f4eb7b" }}> Menu </Nav.Link>
                 <Nav.Link as={Link} smooth to="about-section" duration={50} style={{ cursor: 'pointer', color: "#f4eb7b" }}> About Us </Nav.Link>
                 <Nav.Link as={Link} smooth to="contact-section" duration={50} style={{ cursor: 'pointer', color: "#f4eb7b" }}> Contact Us </Nav.Link>
